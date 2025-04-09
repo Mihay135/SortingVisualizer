ab8f2b3d7a1dd07ee5e2e3b04be78830c9880d5c
Mihai <sauca.mihai.135@gmail.com>
2025-04-09 14:54:01 +0200

    fix component Path resolution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="nav-bar">
-        a
+        
       </div>
       <SortingVisualizer></SortingVisualizer>
     </div>
